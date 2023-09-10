065315cb1edefb2febc0a9c5c3151b57e8429318
vkola <vkolachi@gmail.com>
2023-09-11 00:12:41 +0530

    first

--- library-management-system/src/main/java/com/example/librarymanagementsystem/model/libraryCard.java
+++ library-management-system/src/main/java/com/example/librarymanagementsystem/model/libraryCard.java
@@ -1,5 +1,15 @@
 package com.example.librarymanagementsystem.model;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class libraryCard {
 
 
