9be84826da145f0b2dd2bac5a9ee9a8474550387
vkola <vkolachi@gmail.com>
2023-09-19 18:04:39 +0530

    postgresql and email

--- library-management-system/src/main/java/com/example/librarymanagementsystem/service/Impl/TransactionServiceImpl.java
+++ library-management-system/src/main/java/com/example/librarymanagementsystem/service/Impl/TransactionServiceImpl.java
@@ -15,6 +15,8 @@ import com.example.librarymanagementsystem.repository.StudentRepository;
 import com.example.librarymanagementsystem.repository.TransactionRepository;
 import com.example.librarymanagementsystem.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -28,6 +30,8 @@ public class TransactionServiceImpl implements TransactionService {
     TransactionRepository transactionRepository;
     @Autowired
     BookRepository bookRepository;
+    @Autowired
+    JavaMailSender javaMailSender;
     public  IssueBookResponse issueBook(int bookId, int studentId) {
 
         Optional<Student> studentOptional=studentRepository.findById(studentId);
@@ -62,6 +66,19 @@ public class TransactionServiceImpl implements TransactionService {
 
        Book savedBook= bookRepository.save(book); //book and transaction
         Student savedStudent= studentRepository.save(student); //student and transaction
+
+        //  send an email
+        String text = "Hi! " + student.getName() + " The below book has been issued to you\n" +
+                book.getTitle() + " \nThis is the transaction number: "+savedTransaction.getTransactionNumber();
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("kven241@gmail.com");
+        simpleMailMessage.setTo(student.getEmail());
+        simpleMailMessage.setSubject("Congrats!! Book Issued");
+        simpleMailMessage.setText(text);
+
+        javaMailSender.send(simpleMailMessage);
+
 
         return IssueBookResponse.builder()
                 .transactionNumber(String.valueOf(UUID.randomUUID()))
